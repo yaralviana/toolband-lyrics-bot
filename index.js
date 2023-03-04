162e94b2a1867d510a854db723f9d2c3e8c95114
yara <yaralviana@gmail.com>
2023-03-04 14:51:03 -0300

    alterações

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const Twit = require("twit")
-const genius = require("genius-lyrics-api")
+const lyricsFinder = require("lyrics-finder")
+
 const config = require("./config.js")
 
-// To run project locally
 process.env = config
 
 const client = new Twit({
@@ -12,34 +12,27 @@ const client = new Twit({
   access_token_secret: process.env.access_token_secret,
 })
 
-// Configure the Genius client
-const options = {
-  apiKey: process.env.genius_access_token,
-  title: "",
-  artist: "Tool",
-  optimizeQuery: true,
-}
+const artistName = "Tool"
 
-// List of lyrics for the artist's songs
 let songLyrics = []
 
-// Function to get the lyrics of the artist's songs
 async function getSongLyrics() {
   try {
-    const searchResults = await genius.search(options)
-    const songs = searchResults.map((result) => result.id)
-    const lyrics = await genius.getLyrics(songs, options)
-    songLyrics = lyrics
+    const songs = await lyricsFinder(artistName)
+
+    for (const song of songs) {
+      const lyrics = await lyricsFinder(artistName, song)
+      songLyrics.push(lyrics)
+    }
+
     console.log(songLyrics)
   } catch (error) {
     console.error(error)
   }
 }
 
-// Function to randomly select a song lyric and post it on Twitter
 function postSongLyric() {
-  const songLyric = songLyrics[Math.floor(Math.random() * songLyrics.length)]
-
+  const songLyric = songLyrics[Math.trunc(Math.random() * songLyrics.length)]
   console.log(songLyric)
 
   client.post(
@@ -55,8 +48,7 @@ function postSongLyric() {
   )
 }
 
-getSongLyrics()
-
-// Call the functions at regular intervals
-// setInterval(getSongLyrics, 24 * 60 * 60 * 1000); // Get the lyrics of the songs once a day
-// setInterval(postSongLyric, 2 * 60 * 60 * 1000); // Post a song lyric every two hours
+// getSongLyrics()
+// postSongLyric()
+setInterval(getSongLyrics, 24 * 60 * 60 * 1000)
+setInterval(postSongLyric, 2 * 60 * 60 * 1000) // Postar a cada 2 horas
